9645a65c0ac0ff51caa741e038a0f7897f204296
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2022-04-08 18:31:30 -0300

    feat(Input): create new component

---
+++ components/Input.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="w-100">
+    <div class="flex flex-1 flex-col">
+      <label class="font-bold" :for="id">{{ label }}</label>
+      <input
+        :id="id"
+        :type="type"
+        :name="name"
+        class="bg-input p-2 rounded-lg"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: 'Input',
+    },
+    id: {
+      type: String,
+      default: 'input',
+    },
+    name: {
+      type: String,
+      default: 'name',
+    },
+    type: {
+      type: String,
+      default: 'name',
+    },
+  },
+}
+</script>
