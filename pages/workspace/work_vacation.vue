698a0be9afa7cbf25b6660de95ae2903234c12d5
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2022-04-08 10:59:27 -0300

    create workspace title component

--- pages/workspace/work_vacation.vue
+++ pages/workspace/work_vacation.vue
@@ -1,9 +1,11 @@
 <template>
   <main class="w-full m-5">
     <div class="flex flex-row mt-1">
-      <div class="p-4 m-3 ml-4 mb-0 mt-2 font-bold tracking-wide text-3xl">
-        <h1>Solicitações de Férias</h1>
-      </div>
+      <WorkspaceTitle
+        class="m-3"
+        title="Solicitações de Férias"
+        :button="{ title: 'Nova Solicitação' }"
+      />
     </div>
     <div class="flex h-3/6 max-w-screen-2xl">
       <div class="p-4 m-3 mt-0 bg-card shadow-2xl rounded-lg w-full">
