cddda977892fa864770c4c6ecff3f6b991ea850c
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2022-04-07 22:55:32 -0300

    define layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="h-screen w-100 relative bg-backg">
     <Header class="sticky top-0" />
-    <Nuxt />
+
+    <div class="max-w-screen-xl mx-auto">
+      <Nuxt />
+    </div>
   </div>
 </template>
