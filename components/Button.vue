c583b80f2ad0dd3c02b6b2248f10844d42e27ef0
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2022-04-08 19:36:09 -0300

    feat(Button): create component

---
+++ components/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+    :class="[
+      'font-bold py-2 px-4 rounded',
+      disabled
+        ? 'bg-disabled text-gray-500 cursor-default'
+        : 'text-white bg-blue-500 hover:bg-blue-700',
+    ]"
+    :disabled="disabled"
+    @click="click"
+  >
+    <fa v-if="leftIcon" :icon="['fas', leftIcon]" />
+    <slot />
+    <fa v-if="rightIcon" :icon="['fas', rightIcon]" />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    click: {
+      type: Function,
+      default: () => {},
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    leftIcon: {
+      type: Boolean,
+      default: false,
+    },
+    rightIcon: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
