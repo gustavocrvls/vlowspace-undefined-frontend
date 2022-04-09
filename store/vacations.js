c6e7098f481e2f7a32c2540cf34e52c707b96ec7
Dannspress <danielwalterwork@gmail.com>
2022-04-09 00:13:33 -0300

    Vacations info and modal of validation front-en done

--- store/vacations.js
+++ store/vacations.js
@@ -1,12 +1,45 @@
 export const state = () => ({
   list: [
     {
+      user: {
+        name: 'Gustavo Carvalho',
+      },
+      owner: {
+        name: 'Sim Jr.',
+      },
+      dp: {
+        name: 'Não Jr.',
+      },
+      dateStart: new Date(),
+      dateEnd: new Date(),
+      quantity: 15,
+      status: 'A Validar',
+    },
+    {
+      user: {
+        name: 'Naruto Uzumaki',
+      },
+      owner: {
+        name: 'Kakashi Hatake',
+      },
+      dp: {
+        name: 'Iruka Sensei',
+      },
       dateStart: new Date(),
       dateEnd: new Date(),
       quantity: 15,
       status: 'Aprovado',
     },
     {
+      user: {
+        name: 'Daniel Walter',
+      },
+      owner: {
+        name: 'Shrek da fonseca',
+      },
+      dp: {
+        name: 'Burro da silva',
+      },
       dateStart: new Date(),
       dateEnd: new Date(),
       quantity: 15,
